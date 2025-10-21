7aadcbe0037f435f123a4d7facd992fba11673ed
Hema382 <Hemachowdary0405@gmail.com>
2025-10-21 16:10:59 +0530

    Implement basic navbar

---
+++ app.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav style={{ backgroundColor: '#333', padding: '10px', color: 'white' }}>
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
