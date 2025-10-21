efbd856278210e9ffc8b601ad1edbeaa5e028723
Hema382 <Hemachowdary0405@gmail.com>
2025-10-21 16:12:46 +0530

    Add simple form section

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <nav style={{ backgroundColor: '#333', padding: '10px', color: 'white' }}>
-          <a href="#">Home</a>
-          <a href="#">About</a>
-          <a href="#">Contact</a>
-        </nav>
+        <h2>Simple Form</h2>
+        <input type="text" placeholder="Enter your name" />
+        <button>Submit</button>
       </header>
     </div>
   );
